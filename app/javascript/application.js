0de0fc46866539d95caafecdf2ebd615e5ab636d
Nikolai Bashkirov <bashkirov.office@gmail.com>
2024-07-23 23:34:17 -0700

    Add nav menu and favicon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
+//= require semantic-ui
 import "@hotwired/turbo-rails"
-import "controllers"
+import "controllers" 
+
+$(document).on('turbo:load', function() {
+    $('.ui.dropdown').dropdown();
+})
+// $(document).on('turbo:load', function(){ //     $('.ui.dropdown').dropdown(); // }) 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'chatroom#index'
+
+  get 'signup', to: 'users#new'
+  # A single call to resources can declare all of the necessary routes for your index,
+  #    show, new, edit, create, update, and destroy actions.
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout',to: 'sessions#destroy'
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
