8a89c4da650cc4aba082132858e330984a8f8bdb
Nikolai Bashkirov <bashkirov.office@gmail.com>
2024-07-26 16:45:01 -0700

    Implement realtime messaging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :messages, dependent: :destroy
+
   validates :username, presence: true,
               uniqueness: { case_sensitive: false }, 
               length: { minimum: 3, maximum: 15 }
-  has_secure_password
-  has_many :messages, dependent: :destroy
+  validates :password, presence: true,
+              length: { minimum: 6 },
+              if: -> { new_record? || !password.nil? }
+  validates :password_confirmation, presence: true,
+              if: -> { new_record? || !password.nil? }
+  validate :passwords_match
+
+  private
+
+  def passwords_match
+    errors.add(:password_confirmation, "doesn't match Password") if password != password_confirmation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'chatroom#index'
 
   get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   # A single call to resources can declare all of the necessary routes for your index,
   #    show, new, edit, create, update, and destroy actions.
   resources :users, except: [:new]
@@ -12,6 +13,9 @@ Rails.application.routes.draw do
 
   post 'message', to: 'messages#create'
 
+  mount ActionCable.server, at: '/cable'
+
+  # ================================================================================================
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
